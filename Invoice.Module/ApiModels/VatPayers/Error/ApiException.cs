5e886dd602ccdd0f85b7040a74a263b3d9715fa0
Patryk Wyprzał <pwyprzal@gabos.pl>
2021-10-31 22:08:47 +0100

    Delete unnecessary classes

--- Invoice.Module/ApiModels/VatPayers/Error/ApiException.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Invoice.Module.ApiModels.VatPayers.Error
-{
-    public class ApiException
-    {
-        public string Code { get; set; }
-        public string Message { get; set; }
-    }
-}
