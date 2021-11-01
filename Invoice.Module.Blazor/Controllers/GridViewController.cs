730b75eb3e4f46e317b3149ecab8224c63cdc355
Patryk Wyprzał <pwyprzal@gabos.pl>
2021-11-01 17:53:00 +0100

    GridViewController

---
+++ Invoice.Module.Blazor/Controllers/GridViewController.cs
@@ -0,0 +1,18 @@
+﻿using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Blazor.Editors.Grid;
+
+namespace Invoice.Module.Blazor.Controllers
+{
+    public class GridViewController : ViewController<ListView>
+    {
+        protected override void OnViewControlsCreated()
+        {
+            base.OnViewControlsCreated();
+            if (View.Editor is GridListEditor gridListEditor)
+            {
+                IDxDataGridAdapter dataGridAdapter = gridListEditor.GetDataGridAdapter();
+                dataGridAdapter.DataGridModel.CssClass += " table-striped";
+            }
+        }
+    }
+}
---
+++ Invoice.Module.Win/Controllers/GridViewController.cs
@@ -0,0 +1,22 @@
+﻿using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Win.Editors;
+using DevExpress.XtraGrid.Views.Grid;
+using System.Drawing;
+
+namespace Invoice.Module.Win.Controllers
+{
+    public class GridViewController : ViewController<ListView>
+    {
+        protected override void OnViewControlsCreated()
+        {
+            base.OnViewControlsCreated();
+            if (View.Editor is GridListEditor gridListEditor)
+            {
+                GridView gridView = gridListEditor.GridView;
+                gridView.OptionsView.EnableAppearanceOddRow = true;
+                gridView.Appearance.OddRow.BackColor = Color.FromArgb(244, 244, 244);
+            }
+        }
+    }
+}
+
