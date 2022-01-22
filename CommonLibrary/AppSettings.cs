05440f3e518c7785be08901bf6a7ef05e6610546
Jacek Kosiński <kashiash@gmail.com>
2022-01-22 19:24:12 +0100

    .

--- CommonLibrary/AppSettings.cs
+++ CommonLibrary/AppSettings.cs
@@ -9,7 +9,7 @@
                     return "Integrated Security=SSPI;Pooling=false;Data Source=.;Initial Catalog=Invoice6x";
                 //@"Server=tcp:xafblazorjk.database.windows.net,1433;Initial Catalog=fleetman;Persist Security Info=False;User ID=kashiash;Password=N!....20;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
 
-           
+
             }
 
         }
