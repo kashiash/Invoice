083a240314dfb770acf746480b5c571e8eeb067b
Jacek Kosiński <kashiash@gmail.com>
2022-01-31 12:15:34 +0100

    .

--- Invoice.Module/BusinessObjects/NonPersistent/UploadAssignedFileData.cs
+++ Invoice.Module/BusinessObjects/NonPersistent/UploadAssignedFileData.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 namespace Invoice.Module.BusinessObjects.NonPersistent
 {
     [DomainComponent]
-    [DefaultClassOptions]
+  //  [DefaultClassOptions]
     public class UploadAssignedFileData : NonPersistentBaseObject
     {
         
